03fcf767d2b53215482286d0b00982d3c8b266b8
borkab <borbala.botond@gmail.com>
2022-03-08 11:03:37 +0100

    correcting the test.now it passes

--- StephenGrider/cards/deck_test.go
+++ StephenGrider/cards/deck_test.go
@@ -6,13 +6,6 @@ func TestNewDeck(t *testing.T) {
 	d := newDeck()
 
 	if len(d) != 16 { //16 because we have 4 elemnts in suits and 4 in value
-		t.Errorf("Expected deck length of 20, but got", len(d))
+		t.Errorf("Expected deck length of 16, but got %v", len(d))
 	}
 }
-
-/*
-go test
-# learngo/udemy/StephenGrider/cards
-./deck_test.go:9:11: Errorf call has arguments but no formatting directives
-FAIL    learngo/udemy/StephenGrider/cards [build failed]
-*/
