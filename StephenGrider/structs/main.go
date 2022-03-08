66a91217ecadfcd31c6b8358cd634c1721b43433
borkab <borbala.botond@gmail.com>
2022-03-08 12:52:25 +0100

    make the updateName() but it pops up a fail message at var declaration

--- StephenGrider/structs/main.go
+++ StephenGrider/structs/main.go
@@ -24,10 +24,16 @@ func main() {
 		}, //u have to take a comma "," at the end of every single line
 	}
 
+	jim.updateName("Jimmy")
 	jim.print()
 
 }
 
 func (p person) print() {
 	fmt.Printf("%+v", p)
+}
+
+func (p person) updateName(newFirstName string) {
+	p.firstName = newFirstName
+
 }
