459f212bc08dd0023cb998ec137c31454514b8f2
borkab <borbala.botond@gmail.com>
2022-03-08 10:50:34 +0100

    adding error message

--- StephenGrider/cards/deck_test.go
+++ StephenGrider/cards/deck_test.go
@@ -9,3 +9,10 @@ func TestNewDeck(t *testing.T) {
 		t.Errorf("Expected deck length of 20, but got", len(d))
 	}
 }
+
+/*
+go test
+# learngo/udemy/StephenGrider/cards
+./deck_test.go:9:11: Errorf call has arguments but no formatting directives
+FAIL    learngo/udemy/StephenGrider/cards [build failed]
+*/
