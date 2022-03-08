8faa72ba620fdd81187b69b84e9b51d40a7cdbff
borkab <borbala.botond@gmail.com>
2022-03-08 12:25:36 +0100

    ebedded structs- struct in a struct

--- StephenGrider/structs/main.go
+++ StephenGrider/structs/main.go
@@ -2,22 +2,39 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
-	var mila person
-	var lena person
+	/*
+		var mila person
+		var lena person
 
-	lena.firstName = "Lena"
-	lena.lastName = "Mirena"
+		lena.firstName = "Lena"
+		lena.lastName = "Mirena"
 
-	mia := person{"Mia", "Tenet"}
-	alex := person{firstName: "Alex", lastName: "Anderson"}
-	fmt.Println(mila, lena, mia, alex)
+		mia := person{"Mia", "Tenet"}
+		alex := person{firstName: "Alex", lastName: "Anderson"}
+		fmt.Println(mila, lena, mia, alex)
 
-	fmt.Printf("%+v", lena)
+		fmt.Printf("%+v", lena)
+	*/
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Penaut",
+		contact: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 71544,
+		},
+	}
 
+	fmt.Printf("%+v", jim)
 }
