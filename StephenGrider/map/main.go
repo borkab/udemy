dffa366463e43e0bc9691316b601fddc0cb476fa
borkab <borbala.botond@gmail.com>
2022-03-09 13:20:28 +0100

    how to delete from a map

--- StephenGrider/map/main.go
+++ StephenGrider/map/main.go
@@ -3,20 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	var colors map[string]string
+	//var colors map[string]string
+
+	colors := make(map[string]string)
 
 	/*
 		colors := map[string]string{
-			"red":   "ff0000",
-			"green": "00ff00",
+			"red":   "#ff0000",
+			"green": "#00ff00",
 		}
 	*/
+
+	colors["white"] = "#ffffff"
+
+	delete(colors, "white")
+
 	fmt.Println(colors)
 }
 
-//blue 0000ff
-//yellow ffff00
-//white ffffff
-//black 000000
-//cyan 00ffff
-//magenta ff00ff
+//blue #0000ff
+//yellow #ffff00
+//white #ffffff
+//black #000000
+//cyan #00ffff
+//magenta #ff00ff
