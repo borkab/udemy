823a0ff16fd71bd57bda992d9064e20052998bac
borkab <borbala.botond@gmail.com>
2022-03-09 12:56:45 +0100

    why does the 2.element in the map stand on the first place?

---
+++ StephenGrider/map/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	colors := map[string]string{
+		"red":   "ff0000",
+		"green": "00ff00",
+	}
+	fmt.Println(colors)
+}
+
+//blue 0000ff
+//yellow ffff00
+//white ffffff
+//black 000000
+//cyan 00ffff
+//magenta ff00ff
