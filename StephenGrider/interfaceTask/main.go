c1e4467ef2f47c4daaf55cd80f58b8e614aceb4e
borkab <borbala.botond@gmail.com>
2022-03-10 15:25:32 +0100

    refactor with some cheat

--- StephenGrider/interfaceTask/main.go
+++ StephenGrider/interfaceTask/main.go
@@ -6,26 +6,29 @@ type shape interface {
 	getArea() float64
 }
 
-type triangle struct{}
-type square struct{}
+type triangle struct {
+	base   float64
+	height float64
+}
+
+type square struct {
+	sideLenght float64
+}
 
 func main() {
-	tr := triangle{}
-	sq := square{}
+	tr := triangle{13, 9}
+	sq := square{7}
 
 	printArea(tr)
 	printArea(sq)
 }
 
 func (t triangle) getArea() float64 {
-	base := 13.0
-	height := 9.0
-	return 0.5 * base * height
+	return 0.5 * t.base * t.height
 }
 
 func (s square) getArea() float64 {
-	side := 7
-	return float64(side * side)
+	return s.sideLenght * s.sideLenght
 }
 
 func printArea(s shape) {
