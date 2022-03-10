abca98f554a3e42cf781431b7693528f3cea0400
borkab <borbala.botond@gmail.com>
2022-03-10 14:35:26 +0100

    logWriter struct and Writer() func

--- StephenGrider/http/main.go
+++ StephenGrider/http/main.go
@@ -7,18 +7,26 @@ import (
 	"os"
 )
 
+type logWriter struct{}
+
 func main() {
 	resp, err := http.Get("http://google.com")
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
+
+	lw := logWriter{}
 	/*
 		bs := make([]byte, 99999) //99999 this is a number of the empty elements inside of our byte slice
 		//we need this number because Read() reads only when we still have places in the byte slice
 		resp.Body.Read(bs)
 		fmt.Println(string(bs))
 	*/
-	//this line does exactly the sam as above:
-	io.Copy(os.Stdout, resp.Body)
+	//this line does exactly the same as above:
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	return 1, nil
 }
