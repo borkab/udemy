fabeed7a3c67953190f4a3d51420ab6c5c4b74e7
borkab <borbala.botond@gmail.com>
2022-03-11 10:11:51 +0100

    soluton

--- StephenGrider/interfaceTask2/main.go
+++ StephenGrider/interfaceTask2/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
@@ -24,24 +25,13 @@ What interface types does the File implement?
 If the 'File' type implements the 'Reader' interface, u might
 be able to reuse that io.Copy function!
 */
-type file struct {
-	filename string
-}
 
 func main() {
-	fl := file{filename: os.Args[1]}
-
-	open, err := os.Open(fl.filename)
+	file, err := os.Open(os.Args[1])
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
-	fmt.Println(open)
-	printFileName(fl)
 
-	//io.Copy(fl, open.Body)
-}
-
-func printFileName(f file) string {
-	return (f.filename)
+	io.Copy(os.Stdout, file)
 }
