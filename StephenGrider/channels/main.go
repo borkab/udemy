12c697911acfce91bd9ab2448d388cb08ff1afc6
borkab <borbala.botond@gmail.com>
2022-03-11 14:02:49 +0100

    with channel

--- StephenGrider/channels/main.go
+++ StephenGrider/channels/main.go
@@ -14,17 +14,34 @@ func main() {
 		"http://amazon.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c)
 	}
+
+	for i := 0; i < len(links); i++ { //this prints the data from the channel
+		fmt.Println(<-c) //as many times as long is our slice
+	}
+	/*this is equal to:
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	*/
+
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link) //we don't really need the repsonse what comes back so _
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		c <- "Might be down I think"
 		return
 	}
+	//checkLink() checks the request after and after in the order as u typed them in your slice
 
 	fmt.Println(link, "is up!")
+	c <- "yep it's up!"
 }
