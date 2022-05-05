a6d6f6ecbb6e191e20a4bab0300605e2f5cac934
borkab <borbala.botond@gmail.com>
2022-05-05 08:28:17 +0200

    empty slice because lowercase letter in struct filed name

---
+++ ToddMcLeodd/json/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type person struct {
+	first string
+	last  string
+	age   int
+}
+
+func main() {
+	p1 := person{
+		first: "James",
+		last:  "Bond",
+		age:   32,
+	}
+
+	p2 := person{
+		first: "Miss",
+		last:  "Moneypenny",
+		age:   27,
+	}
+
+	people := []person{p1, p2}
+
+	fmt.Println(people)
+
+	bs, err := json.Marshal(people)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(bs))
+}
