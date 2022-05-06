eb4b76d946fc05e7f3656a45f13df8aaa43c445e
borkab <borbala.botond@gmail.com>
2022-05-06 08:37:16 +0200

    sort by name

--- ToddMcLeodd/func-sort/sort-custom/main.go
+++ ToddMcLeodd/func-sort/sort-custom/main.go
@@ -9,11 +9,11 @@ type Person struct {
 	Name string
 	Age  int
 }
-type ByAge []Person
+type ByName []Person
 
-func (a ByAge) Len() int           { return len(a) }
-func (a ByAge) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a ByAge) Less(i, j int) bool { return a[i].Age < a[j].Age }
+func (bn ByName) Len() int           { return len(bn) }
+func (bn ByName) Swap(i, j int)      { bn[i], bn[j] = bn[j], bn[i] }
+func (bn ByName) Less(i, j int) bool { return bn[i].Name < bn[j].Name }
 
 func (p Person) String() string {
 	return fmt.Sprintf("%s: %d", p.Name, p.Age)
@@ -27,6 +27,6 @@ func main() {
 	people := []Person{p1, p2, p3, p4}
 
 	fmt.Println(people)
-	sort.Sort(ByAge(people))
+	sort.Sort(ByName(people))
 	fmt.Println(people)
 }
