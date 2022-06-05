00a6ad5837a0f30376c09916c1826933be63145c
borkab <borbala.botond@gmail.com>
2022-06-05 23:38:12 +0200

    send only type chan

--- ToddMcLeodd/channels/main.go
+++ ToddMcLeodd/channels/main.go
@@ -3,12 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	c := make(chan int, 2)
+	c := make(chan<- int, 2) //send only
 
 	c <- 42
 	c <- 43
 	fmt.Println(<-c)
-	fmt.Println(<-c)
+	fmt.Println(<-c) //because our channel is a send only type chan
 	fmt.Println("----------------------------------------------------")
 	fmt.Printf("%T\n", c)
 }
