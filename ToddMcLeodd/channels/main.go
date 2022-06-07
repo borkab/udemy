e912548acc3fbbde38f83d90b8c5749c137238b6
borkab <borbala.botond@gmail.com>
2022-06-07 09:18:24 +0200

    .

--- ToddMcLeodd/channels/main.go
+++ ToddMcLeodd/channels/main.go
@@ -8,8 +8,13 @@ func main() {
 	cs := make(chan<- int) //send only
 
 	//because our channel is a send only type chan
-	fmt.Println("----------------------------------------------------")
+	fmt.Println("-------------------")
 	fmt.Printf("c\t%T\n", c)
 	fmt.Printf("cr\t%T\n", cr)
 	fmt.Printf("cs\t%T\n", cs)
+
+	//specific to general doesn't convert
+	fmt.Println("-------------------")
+	fmt.Printf("c\t%T\n", (<-chan int)(c))
+	fmt.Printf("c\t%T\n", (chan<- int)(c))
 }
