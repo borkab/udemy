93566e7591d92d93c214611a08166633e6dd3039
borkab <borbala.botond@gmail.com>
2022-08-05 08:22:26 +0200

    WithCancel with cancel()

--- ToddMcLeodd/context/main.go
+++ ToddMcLeodd/context/main.go
@@ -14,10 +14,21 @@ func main() {
 	fmt.Printf("context type:\t%T\n", ctx)
 	fmt.Println("--------------")
 
-	ctx, _ = context.WithCancel(ctx)
+	ctx, cancel := context.WithCancel(ctx)
 
 	fmt.Println("context:\t", ctx)
 	fmt.Println("context error:\t", ctx.Err())
 	fmt.Printf("context type:\t%T\n", ctx)
+	fmt.Println("cancel:\t\t", cancel)
+	fmt.Printf("cancel type:\t%T\n", cancel)
+	fmt.Println("--------------")
+
+	cancel()
+
+	fmt.Println("context:\t", ctx)
+	fmt.Println("context error:\t", ctx.Err())
+	fmt.Printf("context type:\t%T\n", ctx)
+	fmt.Println("cancel:\t\t", cancel)
+	fmt.Printf("cancel type:\t%T\n", cancel)
 	fmt.Println("--------------")
 }
