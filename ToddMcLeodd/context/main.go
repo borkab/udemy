803987e34d5e9534e63f05e9dab69581183c992d
borkab <borbala.botond@gmail.com>
2022-08-05 08:19:02 +0200

    WithCancel

--- ToddMcLeodd/context/main.go
+++ ToddMcLeodd/context/main.go
@@ -13,4 +13,11 @@ func main() {
 	fmt.Println("context error:\t", ctx.Err())
 	fmt.Printf("context type:\t%T\n", ctx)
 	fmt.Println("--------------")
+
+	ctx, _ = context.WithCancel(ctx)
+
+	fmt.Println("context:\t", ctx)
+	fmt.Println("context error:\t", ctx.Err())
+	fmt.Printf("context type:\t%T\n", ctx)
+	fmt.Println("--------------")
 }
