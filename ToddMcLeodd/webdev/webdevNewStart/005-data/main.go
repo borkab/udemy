e323b4a2ed0e7a7f3999cb013e1fbec771d7f5d7
borkab <borbala.botond@gmail.com>
2023-01-28 23:35:23 +0100

    just practice

---
+++ ToddMcLeodd/webdev/webdevNewStart/005-data/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
+}
+
+func main() {
+	//42 is our data what we want to pass in our template
+	// in the template the syntax is for the current piece of data
+	// is {{.}}
+	err := tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", 42)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
